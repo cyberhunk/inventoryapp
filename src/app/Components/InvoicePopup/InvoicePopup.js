d74a366cc618dfe2386335e3dec54505a3face4a
satyam technology <113225941+cyberhunk@users.noreply.github.com>
2025-11-29 15:36:08 +0530

    Fix price calculation in InvoicePopup component
    hj

--- src/app/Components/InvoicePopup/InvoicePopup.js
+++ src/app/Components/InvoicePopup/InvoicePopup.js
@@ -348,7 +348,10 @@ export default function InvoicePopup({
                           ₹{(item.price - (item.price * 5) / 100).toFixed(2)}
                       </td>
                       <td className="px-3 py-2 text-right border-b border-slate-200">
-                        ₹{item.price * 0.05}
+                        // ₹{item.price * 0.05}
+                      
+                        ₹{(item.price - (item.price * 0.05)).toFixed(2)}
+
                       </td>
                       <td className="px-3 py-2 text-right border-b border-slate-200">
                         ₹{lineTotal}
